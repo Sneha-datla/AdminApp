3f6b03a3f311d75919ea11549bec53165bf5988d
Sneha-datla <datla.sneha1@gmail.com>
2025-06-20 21:03:12 +0530

    made changes

--- Nodejsbackend/db.js
+++ Nodejsbackend/db.js
@@ -1,12 +1,8 @@
-// db.js
-const { Pool } = require('pg');
-
+const { Pool } = require("pg");
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'CreatingApi',
-  password: 'sneha1992',
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
-
 module.exports = pool;
