c51db4a158d0fe48c19b322c5f9fa75b49f96a84
Sneha-datla <datla.sneha1@gmail.com>
2025-06-20 22:51:22 +0530

    changes made

--- AdminApp-main/src/pages/Products.js
+++ AdminApp-main/src/pages/Products.js
@@ -8,7 +8,7 @@ const Products = () => {
   const [featuredFilter, setFeaturedFilter] = useState('All');
   const [products, setProducts] = useState([]);
 
-  const API_URL = 'http://localhost:5000/products/all';
+  const API_URL = 'https://adminapp-1-nk19.onrender.com/products/all';
 
   useEffect(() => {
     fetchProducts();
