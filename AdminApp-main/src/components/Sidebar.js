f9887af672b7388fb0020f019389423fd4f1471f
Sneha-datla <datla.sneha1@gmail.com>
2025-07-07 18:30:43 +0530

    changes made

--- AdminApp-main/src/components/Sidebar.js
+++ AdminApp-main/src/components/Sidebar.js
@@ -15,8 +15,8 @@ const Sidebar = () => (
       <li><Link to="/GoldLoanRequest"><FaUsers style={{ marginRight: '10px' }} />GoldLoanRequest</Link></li>
       <li><Link to="/signup">Signup</Link></li>
       <li><Link to="/Login">Login</Link></li>
-       <li><Link to="/addAddress">addDeliveryAddress</Link></li>
-      <li><Link to="/selectDelivery">Selectaddress</Link></li>
+       <li><Link to="/addDeliveryAddress">addDeliveryAddress</Link></li>
+      <li><Link to="/selectDeliveryAddress">Selectaddress</Link></li>
       <li><Link to="/GoldLoanRequestForm">GoldLoanRequestForm</Link></li>
       <li><Link to="/SellGoldform">SellGoldForm</Link></li>
        
