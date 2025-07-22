de42798cfa6022197b4ede23cdb0b87eb90294bf
Sneha-datla <datla.sneha1@gmail.com>
2025-07-22 22:04:19 +0530

    chnages made

--- Nodejsbackend/firebase.js
+++ Nodejsbackend/firebase.js
@@ -1,10 +1,11 @@
+// firebase.js or config/firebase.js
 const admin = require("firebase-admin");
 
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY, // ✅ no replace here
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     }),
   });
@@ -12,4 +13,4 @@ if (!admin.apps.length) {
 
 const db = admin.firestore();
 
-module.exports = { db, admin };
+module.exports = { db,admin };
