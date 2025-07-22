c06b9cde7fb003f4612fd09bf07452f3a6583e63
Sneha-datla <datla.sneha1@gmail.com>
2025-07-22 17:45:22 +0530

    changes made

--- Nodejsbackend/index.js
+++ Nodejsbackend/index.js
@@ -1,31 +1,23 @@
-// index.js
 const express = require("express");
 const cors = require("cors");
-const admin = require("firebase-admin");
-
 const app = express();
+
+// Routes
 const productRoutes = require("./routes/product");
-const userRoutes = require("./routes/users"); //  import your user route
-const sellGoldRoutes = require("./routes/seller"); //  import your user route
-const ordersRoutes = require("./routes/orders"); //  import your user route
-const goldloanRoutes = require("./routes/goldloan"); //  import your user route
+const userRoutes = require("./routes/users");
+const sellGoldRoutes = require("./routes/seller");
+const ordersRoutes = require("./routes/orders");
+const goldloanRoutes = require("./routes/goldloan");
 
 app.use(cors());
 app.use(express.json());
-const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-
-app.use("/uploads", express.static("uploads")); // serve images
+app.use("/uploads", express.static("uploads"));
 app.use("/products", productRoutes);
-app.use("/users", userRoutes); 
-app.use("/seller", sellGoldRoutes); 
+app.use("/users", userRoutes);
+app.use("/seller", sellGoldRoutes);
 app.use("/order", ordersRoutes);
-app.use("/loan", goldloanRoutes); 
-
-
+app.use("/loan", goldloanRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
--- Nodejsbackend/routes/users.js
+++ Nodejsbackend/routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
-const db = require("firebase-admin").firestore();
+const { db } = require("../firebase"); 
 
 // Signup Route
 router.post("/signup", async (req, res) => {
